0a9f4c7045bc717ea10623a11fe7df8769b4f6de
SANYIZ <42647088+sanyi0802@users.noreply.github.com>
2024-05-12 23:07:43 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import Notification from './Notification';
 import Section from './Section';
+import '../App.css';
 
 const App = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
